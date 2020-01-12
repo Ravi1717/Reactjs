50f684fa0f1bcb0c944a3c4e3d19545f305591cc
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-01-12 23:23:44 +0530

    functioncomponent props

---
+++ Clock.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Clock(props) {
+    return (
+        <div>
+            <h1>The time is :</h1>
+            <h2> {props.date.toLocaleTimeString()}</h2>
+        </div>
+    )
+}
+function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')
+    );
+}
+setInterval(tick, 1000);

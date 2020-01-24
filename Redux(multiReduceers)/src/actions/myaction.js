0b64d57ce164fb9e07fc10f2102adff6da01dee7
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-01-25 00:53:28 +0530

    Combing Multiple Reducers In Redux

---
+++ Redux(multiReduceers)/src/actions/myaction.js
@@ -0,0 +1,29 @@
+/*export const anotherName = (name) => {
+    return {
+        type: 'CHANGE_NAME',
+        payload: name                              ///ACTION CREATORS
+    }                                                                    //ASYNCHRONOUS OPERATION
+}
+*/
+
+
+export const anotherName = () => {
+    return (dispatch) => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(res2 => {
+                dispatch({ type: 'CHANGE_NAME', payload: res2[0].name })
+            })
+
+    }
+}
+
+
+
+
+export const addwish = () => {
+    return {
+        type: 'add_wish',
+        payload: 'Play'
+    }
+}
---
+++ Redux(multiReduceers)/src/reducers/nameReducer.js
@@ -0,0 +1,19 @@
+/*
+const istate = {
+    name: 'ramesh',
+    wishes: ['eat', 'code']
+}
+*/
+
+
+const nameReducer = (state = ' ', action) => {
+    //console.log(action)
+    // return state;
+
+    if (action.type === 'CHANGE_NAME') {
+        return action.payload
+    }
+    return state;
+}
+
+export default nameReducer;
